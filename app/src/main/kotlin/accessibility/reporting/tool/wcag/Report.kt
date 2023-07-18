50804b21f54b9b67bf812b21c2e73eb1c356e20a
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2023-07-18 10:29:23 +0200

    Report model

--- app/src/main/kotlin/accessibility/reporting/tool/wcag/Report.kt
+++ app/src/main/kotlin/accessibility/reporting/tool/wcag/Report.kt
@@ -2,7 +2,8 @@ package accessibility.reporting.tool.wcag
 
 import java.time.LocalDate
 
-abstract class Report(
+
+abstract class BaseReport(
     val url: String,
     val team: Team,
     val version: Version,
@@ -11,18 +12,50 @@ abstract class Report(
     val testpersonIdent: String? = null,
 )
 
+class Team(val name: String, teamOrganization: TeamOrganization? = null)
+class TeamOrganization(val name: String)
+
+enum class Version() { ONE }
+
+enum class Status() {
+    COMPLIANT, NON_COMPLIANT, NOT_APPLICABLE, NOT_TESTED
+}
+
+class Guideline(val name: String, val section: Int, val principle: Principle)
+
+enum class Principle(val number: Int, description: String) {
+    PERCEIVABLE(
+        1,
+        "Information and user interface components must be presentable to users in ways they can perceive."
+    ),
+    OPERABLE(2, "User interface components and navigation must be operable"),
+    UNDERSTANDABLE(3, "Information and the operation of user interface must be understandable"),
+    ROBUST(
+        4,
+        "Content must be robust enough that it can be interpreted by by a wide variety of user agents, including assistive technologies"
+    )
+}
+
 class SuccessCriterion(
     val name: String,
     val guideline: Guideline,
     val number: Int,
+    var status: Status,
+    wcagUrl: String,
+    helpUrl: String,
     val deviations: List<Deviation>? = null
 ) {
-    val status = when {
-        deviations == null -> Status.NOT_TESTED
-        deviations.isEmpty() -> Status.COMPLIANT
-        deviations.all { it.correctedDate != null } -> Status.COMPLIANT
-        deviations.any { it.correctedDate == null } -> Status.NON_COMPLIANT
-        else -> throw IllegalStateException("This should not happen")
+    val successCriterionNumber = "${guideline.principle.number}.${guideline.section}.${this.number}"
+
+    companion object {
+        fun createEmpty(
+            name: String,
+            guideline: Guideline,
+            number: Int,
+            wcagUrl: String,
+            helpUrl: String = "https://aksel.nav.no/god-praksis/universell-utforming"
+        ): SuccessCriterion =
+            SuccessCriterion(name, guideline, number, Status.NOT_TESTED, wcagUrl, helpUrl)
     }
 }
 
@@ -31,18 +64,3 @@ class Deviation(
     val description: String,
     val correctedDate: LocalDate? = null,
 )
-
-
-class Team(val name: String, teamOrganization: TeamOrganization? = null)
-class TeamOrganization(val name: String)
-
-enum class Version() {
-    ONE
-}
-
-enum class Status() {
-    COMPLIANT, NON_COMPLIANT, NOT_TESTED
-}
-
-abstract class Principle(val name: String, val number: Int)
-abstract class Guideline(val name: String, val number: Int, val principle: Principle)
---
+++ app/src/main/kotlin/accessibility/reporting/tool/wcag/Version1.kt
@@ -0,0 +1,23 @@
+package accessibility.reporting.tool.wcag
+
+
+class Version1Report(
+    url: String,
+    team: Team,
+    version: Version,
+    testUrl: String? = null,
+    successCriteria: List<SuccessCriterion>,
+    testpersonIdent: String? = null
+) : BaseReport(url, team, version, testUrl, successCriteria, testpersonIdent) {
+
+    companion object {
+        val textAlternatives = Guideline(name = "Text Alternatives", section = 1, principle = Principle.PERCEIVABLE)
+        val timebasedMedia = Guideline(name = "Timebased media", section = 2, principle = Principle.PERCEIVABLE)
+        val adaptable = Guideline(name = "Adatptable", section = 3, principle = Principle.PERCEIVABLE)
+        val distinguishable = Guideline(name = "Distinguishable", section = 3, principle = Principle.PERCEIVABLE)
+        val successCriteriaV1 = listOf(
+            SuccessCriterion.createEmpty(name = "Non-text content", guideline = textAlternatives, number = 1, "https://www.w3.org/TR/WCAG21/#non-text-content")
+        )
+    }
+}
+
