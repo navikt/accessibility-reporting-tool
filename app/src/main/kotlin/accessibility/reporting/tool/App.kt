a780f793649bfc3aea22d09739795ce1f6f72de0
Carl Hedgren <carl.hedgren@nav.no>
2023-07-19 15:30:33 +0200

    layouting

--- app/src/main/kotlin/accessibility/reporting/tool/App.kt
+++ app/src/main/kotlin/accessibility/reporting/tool/App.kt
@@ -16,13 +16,18 @@ import kotlinx.html.*
 import kotlinx.html.stream.createHTML
 import kotlinx.css.*
 
-fun FlowContent.disclosureArea(sc: SuccessCriterion, summary: String, dataName: String) {
+fun FlowContent.disclosureArea(sc: SuccessCriterion, summary: String, description: String, dataName: String) {
     details {
         summary {
             +"${summary}"
         }
         div {
+            label {
+                htmlFor = "${sc.successCriterionNumber}-${dataName}"
+                +"${description}"
+            }
             textArea {
+                id = "${sc.successCriterionNumber}-${dataName}"
                 hxTrigger("keyup changed delay:1500ms")
                 hxPost("/submit")
                 hxVals("""{"index": "${sc.successCriterionNumber}"}""")
@@ -31,6 +36,7 @@ fun FlowContent.disclosureArea(sc: SuccessCriterion, summary: String, dataName: 
                 rows = "10"
                 placeholder = "Leave blank if you're not breaking the law"
             }
+
         }
 
     }
@@ -66,20 +72,29 @@ fun FlowContent.a11yForm(sc: SuccessCriterion) {
                 hxSwapOuter()
                 attributes["name"] = "status"
                 attributes["hx-vals"] = """{"index": "${sc.successCriterionNumber}"}"""
-                legend { +"Oppfyller alt innhold i testsettet kravet?"}
-                statusRadio(sc,"compliant", Status.COMPLIANT, "Ja")
-                statusRadio(sc,"non compliant", Status.NON_COMPLIANT, "Nej")
-                statusRadio(sc,"not tested", Status.NOT_TESTED, "Ikke testet")
+                legend { +"Oppfyller alt innhold i testsettet kravet?" }
+                statusRadio(sc, "compliant", Status.COMPLIANT, "Ja")
+                statusRadio(sc, "non compliant", Status.NON_COMPLIANT, "Nej")
+                statusRadio(sc, "not tested", Status.NOT_TESTED, "Ikke testet")
                 statusRadio(sc, "not applicable", Status.NOT_APPLICABLE, "Vi har ikke denne typen av innhold")
             }
         }
         if (sc.status == Status.NON_COMPLIANT) {
             div {
-                disclosureArea(sc, "Det er innhold i testsettet som bryter kravet.", "breaking-the-law")
-                disclosureArea(sc, "Det er innhold i testsettet som ikke er underlagt kravet.", "law-does-not-apply")
+                disclosureArea(
+                    sc,
+                    "Det er innhold i testsettet som bryter kravet.",
+                    "Beskriv kort hvilket innhold som bryter kravet, hvorfor og konsekvensene dette får for brukeren.",
+                    "breaking-the-law"
+                )
+                disclosureArea(
+                    sc, "Det er innhold i testsettet som ikke er underlagt kravet.",
+                    "Hvilket innhold er ikke underlagt kravet?", "law-does-not-apply"
+                )
                 disclosureArea(
                     sc,
                     "Innholdet er unntatt fordi det er en uforholdsmessig stor byrde å følge kravet.",
+                    "Hvorfor mener vi at det er en uforholdsmessig stor byrde for innholdet å følge kravet?",
                     "too-hard-to-comply"
                 )
             }
@@ -127,7 +142,7 @@ fun Application.api() {
                     )
                 }
             } ?: run {
-                call.respond(HttpStatusCode.NotFound)
+                call.respond(HttpStatusCode.NotFound, "ENOENT")
             }
         }
         get("/index.html") {
@@ -153,6 +168,11 @@ fun Application.api() {
                 }
                 body {
                     main {
+                        h1 { +"A11y report" }
+                        label {
+                            +"Url:"
+                            input { type = InputType.text }
+                        }
                         Version1Report.successCriteriaV1.map { a11yForm(it) }
                     }
                 }
