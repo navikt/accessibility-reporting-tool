7ce8cde6d6186fffa7ec6a7ef684daf5222452a4
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2023-07-20 17:00:38 +0200

    legg til orgroutes

--- app/src/main/kotlin/accessibility/reporting/tool/App.kt
+++ app/src/main/kotlin/accessibility/reporting/tool/App.kt
@@ -19,25 +19,21 @@ import io.ktor.server.routing.*
 import kotlinx.html.*
 import kotlinx.html.stream.createHTML
 import kotlinx.css.*
+import kotliquery.param
 import java.lang.IllegalArgumentException
 
 fun SuccessCriterion.cssClass() =
     "f" + this.successCriterionNumber.replace(".", "-")
 
-val testOrg = OrganizationUnit("carls-awesome-test-unit", "Carls awesome test unit", email = "awesom@nav.no")
+val testOrg = OrganizationUnit("carls-awesome-test-unit", "Carls awesome test unit")
 
 fun main() {
     val environment = Environment()
     Flyway.runFlywayMigrations(Environment())
-    val repository = ReportRepository(PostgresDatabase(environment)).also { reportRepository ->
+    val repository = ReportRepository(PostgresDatabase(environment)).also {reportRepository ->
         //id som kan brukes når du skal sette opp rapporter: "carls-awesome-test-unit"
-        reportRepository.insertOrganizationUnit(
-            OrganizationUnit(
-                id = "carls-awesome-test-unit",
-                name = "Carls awesome test unit",
-                email = "awesome@nav.no"
-            )
-        )
+
+        reportRepository.insertOrganizationUnit(OrganizationUnit("carls-awesome-test-unit", "Carls awesome test unit"))
 
     }
 
@@ -71,9 +67,9 @@ fun Application.api(repository: ReportRepository, authInstaller: Application.() 
             val status = formParameters["status"].toString()
             val index = formParameters["index"].toString()
             val filters = listOf(formParameters["multimedia-filter"],
-                            formParameters["form-filter"],
-                            formParameters["timelimit-filter"],
-                            formParameters["interaction-filter"]).map { it.toString() }
+                formParameters["form-filter"],
+                formParameters["timelimit-filter"],
+                formParameters["interaction-filter"]).map { it.toString() }
 
             val report = ReportV1.successCriteriaV1.find { it.successCriterionNumber == index }
             report?.let { foundReport ->
@@ -98,52 +94,53 @@ fun Application.api(repository: ReportRepository, authInstaller: Application.() 
                 call.respond(HttpStatusCode.NotFound, "ENOENT")
             }
         }
-        get("/{id}") {
+
+        get("/index.html") {
+            call.respondHtml(HttpStatusCode.OK) {
+                lang = "no"
+                head {
+                    headContent("Select reports")
+                }
+                body {
+                    h1 { +"Select a report"}
+                    a { href="report/foo"
+                        +"there's only this one, sir"
+                    }
+                }
+            }
+        }
+        get("/report/{id}") {
             val id = call.parameters["id"]?: throw IllegalArgumentException()
             val report = repository.getReport(id)?: Report.createLatest("url", testOrg, "foo", null  )
 
             call.respondHtml(HttpStatusCode.OK) {
                 lang = "no"
                 head {
-                    meta { charset = "UTF-8" }
-                    style {
-                    }
-                    title { +"Accessibility reporting ${report.organizationUnit.name} " }
-                    script { src = "https://unpkg.com/htmx.org/dist/htmx.js" }
-
-                    link {
-                        rel = "preload"
-                        href = "https://cdn.nav.no/aksel/@navikt/ds-css/2.9.0/index.min.css"
-                        attributes["as"] = "style"
-                    }
-                    link {
-                        rel = "stylesheet"
-                        href = "static/style.css"
-
-                    }
+                    headContent("A11y report")
                 }
                 body {
                     p {+"${report.organizationUnit.name}"}
                     main {
                         h1 { +"A11y report" }
-                        p { +"Hvem fyller ut rapporten?" }
-                        p { +"Fyller du ut rapporten på vegne av et annet team?" }
-                        p { +"Kontaktperson fra det andre teamet" }
-                        h2 { +"Om løsningen" }
+                        p { +"Hvem fyller ut rapporten?"}
+                        p { +"Fyller du ut rapporten på vegne av et annet team?"}
+                        p { +"Kontaktperson fra det andre teamet"}
+                        h2 {+"Om løsningen" }
                         p { +"Hva heter løsningen?" }
                         p { +"Løsningens base-URL" }
-                        p { +"(For PoC'en) URLen som er testet" }
-                        label {
-                            +"Url:"
-                            input { type = InputType.text }
-                        }
-                        label {
-                            +"Multimedia: Har sidene du skal teste multimedia eller innhold som flasher, f.eks. video, lydfiler, animasjoner?"
-                            input {
-                                type = InputType.checkBox
-                                value = "multimedia-filter"
-                                name = "multimedia-filter"
-                                attributes["data-removes"] = """ removes
+                        p { +"(For PoC'en) URLen som er testet"}
+                        div {
+                            label {
+                                +"Url:"
+                                input { type = InputType.text }
+                            }
+                            label {
+
+                                input {
+                                    type = InputType.checkBox
+                                    value = "multimedia-filter"
+                                    name = "multimedia-filter"
+                                    attributes["data-removes"] = """ removes
                                 1.2.1 Bare lyd og bare video
                                 1.2.2 Teksting (forhåndsinnspilt)
                                 1.2.3 Synstolking eller mediealternativ (forhåndsinnspilt)
@@ -151,15 +148,16 @@ fun Application.api(repository: ReportRepository, authInstaller: Application.() 
                                 1.4.2 Styring av lyd
                                 2.3.1 Terskelverdi på maksimalt tre glimt
                                 """
+                                }
+                                +"Multimedia: Har sidene du skal teste multimedia eller innhold som flasher, f.eks. video, lydfiler, animasjoner?"
                             }
-                        }
-                        label {
-                            +"Skjemaer: Har løsningen din skjemafelter (utenom i dekoratøren), eller mottar løsningen inndata fra brukeren?"
-                            input {
-                                type = InputType.checkBox
-                                name = "form-filter"
-                                value = "form-filter"
-                                attributes["data-removes"] = """ removes
+                            label {
+
+                                input {
+                                    type = InputType.checkBox
+                                    name = "form-filter"
+                                    value = "form-filter"
+                                    attributes["data-removes"] = """ removes
 
                              1.3.5 Identifiser formål med inndata
                              2.5.3 Ledetekst i navn
@@ -169,32 +167,36 @@ fun Application.api(repository: ReportRepository, authInstaller: Application.() 
                              3.3.3 Forslag ved feil
                              3.3.4 Forhindring av feil
                             """
+                                }
+                                +"Skjemaer: Har løsningen din skjemafelter (utenom i dekoratøren), eller mottar løsningen inndata fra brukeren?"
                             }
-                        }
 
-                        label {
-                            +"Interaksjonsmønstre: Har du bevegelsesaktivert innhold, hurtigtaster, eller gestures?"
-                            input {
-                                type = InputType.checkBox
-                                name = "interaction-filter"
-                                value = "interaction-filter"
-                                attributes["data-removes"] = """ removes
+                            label {
+
+                                input {
+                                    type = InputType.checkBox
+                                    name = "interaction-filter"
+                                    value = "interaction-filter"
+                                    attributes["data-removes"] = """ removes
                         2.1.4 Hurtigtaster som består av ett tegn
                         2.5.1 Pekerbevegelser
                         2.5.4 Bevegelsesaktivering
                         """
+                                }
+                                +"Interaksjonsmønstre: Har du bevegelsesaktivert innhold, hurtigtaster, eller gestures?"
                             }
-                        }
-                        label {
-                            +"Tidsbegrensninger og innhold som oppdaterer seg automatisk: Har du innhold med tidsbegrensning? F.eks. automatisk utlogging, begrenset tid til å ta en quiz."
-                            input {
-                                type = InputType.checkBox
-                                value = "timelimit-filter"
-                                name = "timelimit-filter"
-                                attributes["data-removes"] = """ removes
+                            label {
+
+                                input {
+                                    type = InputType.checkBox
+                                    value = "timelimit-filter"
+                                    name = "timelimit-filter"
+                                    attributes["data-removes"] = """ removes
                                  2.2.1 Justerbar hastighet
                                  2.2.2 Pause, stopp, skjul
                                """
+                                }
+                                +"Tidsbegrensninger og innhold som oppdaterer seg automatisk: Har du innhold med tidsbegrensning? F.eks. automatisk utlogging, begrenset tid til å ta en quiz."
                             }
                         }
                         ReportV1.successCriteriaV1.map { a11yForm(it) }
