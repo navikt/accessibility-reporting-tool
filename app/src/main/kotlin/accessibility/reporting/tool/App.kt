e25c716e5a86973547f09936a97bffb693fcaa0b
Carl Hedgren <carl.hedgren@nav.no>
2023-07-20 15:51:29 +0200

    Reactor out the html extension functions

--- app/src/main/kotlin/accessibility/reporting/tool/App.kt
+++ app/src/main/kotlin/accessibility/reporting/tool/App.kt
@@ -22,91 +22,8 @@ import kotlinx.css.*
 import kotliquery.param
 import java.lang.IllegalArgumentException
 
-fun FlowContent.disclosureArea(sc: SuccessCriterion, summary: String, description: String, dataName: String) {
-    details {
-        summary {
-            +"${summary}"
-        }
-        div {
-            label {
-                htmlFor = "${sc.successCriterionNumber}-${dataName}"
-                +"${description}"
-            }
-            textArea {
-                id = "${sc.successCriterionNumber}-${dataName}"
-                hxTrigger("keyup changed delay:1500ms")
-                hxPost("/submit")
-                hxVals("""{"index": "${sc.successCriterionNumber}"}""")
-                name = dataName
-                cols = "80"
-                rows = "10"
-                placeholder = "Leave blank if you're not breaking the law"
-            }
-
-        }
-
-    }
-}
-
 fun SuccessCriterion.cssClass() =
     "f" + this.successCriterionNumber.replace(".", "-")
-
-fun FIELDSET.statusRadio(sc: SuccessCriterion, value_: String, status: Status, display: String) {
-    label {
-        input {
-
-            type = InputType.radio
-            if (sc.status == status) {
-                checked = true
-            }
-            value = value_
-            name = "status"
-        }
-        +"${display}"
-    }
-}
-
-fun FlowContent.a11yForm(sc: SuccessCriterion) {
-    form(classes = "${sc.cssClass()}") {
-        h2 { +"${sc.successCriterionNumber} ${sc.name}" }
-        div {
-
-            fieldSet {
-                hxPost("/submit")
-                hxTarget(".${sc.cssClass()}")
-                hxSelect("form")
-                hxSwapOuter()
-                attributes["name"] = "status"
-                attributes["hx-vals"] = """{"index": "${sc.successCriterionNumber}"}"""
-                legend { +"Oppfyller alt innhold i testsettet kravet?" }
-                statusRadio(sc, "compliant", Status.COMPLIANT, "Ja")
-                statusRadio(sc, "non compliant", Status.NON_COMPLIANT, "Nej")
-                statusRadio(sc, "not tested", Status.NOT_TESTED, "Ikke testet")
-                statusRadio(sc, "not applicable", Status.NOT_APPLICABLE, "Vi har ikke denne typen av innhold")
-            }
-        }
-        if (sc.status == Status.NON_COMPLIANT) {
-            div {
-                disclosureArea(
-                    sc,
-                    "Det er innhold i testsettet som bryter kravet.",
-                    "Beskriv kort hvilket innhold som bryter kravet, hvorfor og konsekvensene dette får for brukeren.",
-                    "breaking-the-law"
-                )
-                disclosureArea(
-                    sc, "Det er innhold i testsettet som ikke er underlagt kravet.",
-                    "Hvilket innhold er ikke underlagt kravet?", "law-does-not-apply"
-                )
-                disclosureArea(
-                    sc,
-                    "Innholdet er unntatt fordi det er en uforholdsmessig stor byrde å følge kravet.",
-                    "Hvorfor mener vi at det er en uforholdsmessig stor byrde for innholdet å følge kravet?",
-                    "too-hard-to-comply"
-                )
-            }
-        }
-    }
-}
 
 val testOrg = OrganizationUnit("carls-awesome-test-unit", "Carls awesome test unit")
 
@@ -126,7 +43,6 @@ fun main() {
 fun Application.installAzure() {
     install(Authentication) {
         jwt {}
-        verifier()
     }
 }
 suspend inline fun ApplicationCall.respondCss(builder: CssBuilder.() -> Unit) {
@@ -203,6 +119,7 @@ fun Application.api(repository: ReportRepository, authInstaller: Application.() 
                     }
                 }
                 body {
+                    p {+"${report.organizationUnit.name}"}
                     main {
                         h1 { +"A11y report" }
                         p { +"Hvem fyller ut rapporten?"}
---
+++ app/src/main/kotlin/accessibility/reporting/tool/microfrontends.kt
@@ -0,0 +1,90 @@
+package accessibility.reporting.tool
+
+import accessibility.reporting.tool.wcag.Status
+import accessibility.reporting.tool.wcag.SuccessCriterion
+import kotlinx.html.*
+
+
+fun FlowContent.disclosureArea(sc: SuccessCriterion, summary: String, description: String, dataName: String) {
+    details {
+        summary {
+            +"${summary}"
+        }
+        div {
+            label {
+                htmlFor = "${sc.successCriterionNumber}-${dataName}"
+                +"${description}"
+            }
+            textArea {
+                id = "${sc.successCriterionNumber}-${dataName}"
+                hxTrigger("keyup changed delay:1500ms")
+                hxPost("/submit")
+                hxVals("""{"index": "${sc.successCriterionNumber}"}""")
+                name = dataName
+                cols = "80"
+                rows = "10"
+                placeholder = "Leave blank if you're not breaking the law"
+            }
+
+        }
+
+    }
+}
+
+
+fun FIELDSET.statusRadio(sc: SuccessCriterion, value_: String, status: Status, display: String) {
+    label {
+        input {
+
+            type = InputType.radio
+            if (sc.status == status) {
+                checked = true
+            }
+            value = value_
+            name = "status"
+        }
+        +"${display}"
+    }
+}
+
+fun FlowContent.a11yForm(sc: SuccessCriterion) {
+    form(classes = "${sc.cssClass()}") {
+        h2 { +"${sc.successCriterionNumber} ${sc.name}" }
+        div {
+
+            fieldSet {
+                hxPost("/submit")
+                hxTarget(".${sc.cssClass()}")
+                hxSelect("form")
+                hxSwapOuter()
+                attributes["name"] = "status"
+                attributes["hx-vals"] = """{"index": "${sc.successCriterionNumber}"}"""
+                legend { +"Oppfyller alt innhold i testsettet kravet?" }
+                statusRadio(sc, "compliant", Status.COMPLIANT, "Ja")
+                statusRadio(sc, "non compliant", Status.NON_COMPLIANT, "Nej")
+                statusRadio(sc, "not tested", Status.NOT_TESTED, "Ikke testet")
+                statusRadio(sc, "not applicable", Status.NOT_APPLICABLE, "Vi har ikke denne typen av innhold")
+            }
+        }
+        if (sc.status == Status.NON_COMPLIANT) {
+            div {
+                disclosureArea(
+                    sc,
+                    "Det er innhold i testsettet som bryter kravet.",
+                    "Beskriv kort hvilket innhold som bryter kravet, hvorfor og konsekvensene dette får for brukeren.",
+                    "breaking-the-law"
+                )
+                disclosureArea(
+                    sc, "Det er innhold i testsettet som ikke er underlagt kravet.",
+                    "Hvilket innhold er ikke underlagt kravet?", "law-does-not-apply"
+                )
+                disclosureArea(
+                    sc,
+                    "Innholdet er unntatt fordi det er en uforholdsmessig stor byrde å følge kravet.",
+                    "Hvorfor mener vi at det er en uforholdsmessig stor byrde for innholdet å følge kravet?",
+                    "too-hard-to-comply"
+                )
+            }
+        }
+    }
+}
