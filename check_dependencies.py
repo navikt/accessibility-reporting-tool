a3955e3d3bc0f5fc391d524d4e4bbf9848c5bd28
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2024-07-09 11:11:24 +0200

    gradle sjekk dependencies

---
+++ check_dependencies.py
@@ -0,0 +1,46 @@
+# importing the module
+import json
+import subprocess
+import re
+from datetime import date
+
+
+json_file_name = 'build/dependencyUpdates/dependencies.json'
+dependency_definition_file = "buildSrc/src/main/kotlin/dependencies/Groups.kt"
+
+def run_checks():
+    subprocess.call(["./gradlew", "dependencyUpdates"])
+    updates = list(get_avaiable_updates())
+    if len(updates) != 0:
+        updates_summary = [map_dependency(dep) for dep in updates if is_major_version(dep)]
+        print(f'Found {len(updates_summary)} outdated dependencies')
+        write_as_comment(updates_summary)
+
+
+
+def get_avaiable_updates():
+    with open(json_file_name, 'r') as json_file:
+        data = json.load(json_file)
+        available_updates = data["outdated"]["dependencies"]
+        json_file.close()
+        return available_updates
+
+
+def is_major_version(version):
+    return bool(re.search("^[0-9.]*$", version["available"]["milestone"]))
+
+
+def map_dependency(dep):
+    return f'{dep["group"]}:  {dep["version"]} -> {dep["available"]["milestone"]}'
+
+def write_as_comment(pending_updates):
+    dependency_file = open(dependency_definition_file, 'a')
+    dependency_file.write("\n/*\n")
+    dependency_file.write(f'{date.today()}: {len(pending_updates)} outdated dependencies')
+    for pending in pending_updates:
+        dependency_file.write(f'\n{pending}')
+    dependency_file.write("\n*/")
+    dependency_file.close()
+
+
+run_checks()
