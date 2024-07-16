59223ba660a6cf5d109bc45a2ce5a1034eb93268
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2024-07-16 12:40:47 +0200

    flere felter i user/details

--- app/src/main/kotlin/accessibility/reporting/tool/rest/UserRoutes.kt
+++ app/src/main/kotlin/accessibility/reporting/tool/rest/UserRoutes.kt
@@ -7,26 +7,49 @@ import accessibility.reporting.tool.database.OrganizationRepository
 import accessibility.reporting.tool.microfrontends.reportListItem
 import accessibility.reporting.tool.wcag.Report
 import accessibility.reporting.tool.microfrontends.reportListItem
+import accessibility.reporting.tool.rest.ReportInfo.Companion.toReportInfo
 import accessibility.reporting.tool.wcag.OrganizationUnit
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 fun Route.jsonApiUsers(repository: ReportRepository, repo: OrganizationRepository) {
     route("users") {
-    get("details") {
+        get("details") {
 
-        val reports = repository.getReportsForUser(call.user.oid)
-            .sortedBy { it.descriptiveName?.lowercase()?:it.url }
-        val teams = repo.getOrganizationForUser(call.user.email)
-        val userDetails = UserDetails(email = call.user.email.str(), reports = reports, teams = teams)
-        call.respond(userDetails)
-    }}
+            val reports = repository.getReportsForUser(call.user.oid)
+                .sortedBy { it.descriptiveName?.lowercase() ?: it.url }
+                .map { it.toReportInfo() }
+            val teams = repo.getOrganizationForUser(call.user.email)
+            val userDetails =
+                UserDetails(name = call.user.username, email = call.user.email.str(), reports = reports, teams = teams)
+            call.respond(userDetails)
+        }
+    }
 }
+
 data class UserDetails(
+    val name: String,
     val email: String,
-    val reports: List<Report>,
-    val teams: List<String>
+    val reports: List<ReportInfo>,
+    val teams: List<OrganizationUnit>
 )
 
+data class ReportInfo(val title: String, val id: String, val teamId: String, val date: String) {
+    companion object {
+        fun Report.toReportInfo() = ReportInfo(
+            title = descriptiveName ?: this.url,
+            id = reportId,
+            teamId = organizationUnit?.id ?: "",
+            date = "yyyy-MM-dd".datestr(lastChanged)
+        )
+    }
+}
+
+private fun String.datestr(date: LocalDateTime) = let {
+    val formatter = DateTimeFormatter.ofPattern(this)
+    date.format(formatter)
+}
