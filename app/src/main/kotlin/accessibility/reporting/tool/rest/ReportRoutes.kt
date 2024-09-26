e0c42551faaba70bc3c721e3e5a86139dcba0681
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2024-09-26 08:19:21 +0200

    Fix feil i test

--- app/src/main/kotlin/accessibility/reporting/tool/rest/ReportRoutes.kt
+++ app/src/main/kotlin/accessibility/reporting/tool/rest/ReportRoutes.kt
@@ -71,8 +71,8 @@ fun Route.jsonApiReports(reportRepository: ReportRepository, organizationReposit
                     author = updates.author,
                     lastChanged = LocalDateTimeHelper.nowAtUtc(),
                     lastUpdatedBy = call.user.toAuthor(),
-                    notes = updates.notes
-                    isPartOfNavNo = updates.isPartOfNavNo
+                    notes = updates.notes,
+                    isPartOfNavNo = updates.isPartOfNavNo ?: existingReport.isPartOfNavNo
                 )
 
                 updates.successCriteria?.let { pendingUpdateList ->
